fc62733c4761902418a0bacd4cf2f2439a1b7f96
bhddnrsy <bhddnrsy@gmail.com>
2013-08-04 15:05:39 +0300

    main frame constructed

---
+++ maze/src/Maze/Maze.java
@@ -0,0 +1,21 @@
+package Maze;
+
+import javax.swing.*;
+
+public class Maze {
+	public static void main(String[] args){
+		new Maze();
+	}
+	
+	public Maze(){
+		JFrame fr = new JFrame();
+		fr.setTitle("Maze Game");
+		fr.setSize(500,400);
+		fr.setLocationRelativeTo(null);
+		//fr.add(new Board());
+		fr.setVisible(true);
+		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+}
